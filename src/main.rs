a5c6598516b2e71d774950c014d7e3f151242dfd
devguchi <devguchitaro@gmail.com>
2021-10-19 20:05:48 +0900

    added not found

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::fs::File;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -14,11 +14,24 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    let mut file = File::open("a.html").unwrap();
+    println!("{}", String::from_utf8_lossy(&buffer[..]));
+
+    let get = b"GET / HTTP/1.1\r\n";
     let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    let mut status_code = 200;
+    let mut status_msg = "OK";
+    if buffer.starts_with(get) {
+        let mut file = File::open("a.html").unwrap();
+        file.read_to_string(&mut contents).unwrap();
+    } else {
+        status_code = 400;
+        status_msg = "NOT FOUND";
+        contents = String::from("NOT FOUND");
+    }
+    let response = format!(
+        "HTTP/1.1 {} {}\r\n\r\n{}",
+        status_code, status_msg, contents
+    );
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
